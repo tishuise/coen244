7fb4d0e7dece2cf628d3137d5fa19fa931aa485b
Tyra <amaabankwa@gmail.com>
2026-02-13 22:46:16 -0500

    Step 2: Added Dog and Cat with override

--- main.cpp
+++ main.cpp
@@ -6,3 +6,22 @@ class Animal {
     public:
         virtual void speak() { cout << "Animal makes a sound\n"; }
 };
+
+class Dog : public Animal {
+
+    public:
+        void speak() final override { cout << "Dog barks\n"; }
+};
+
+class Cat : public Animal {
+
+    public:
+        void speak() override { cout << "Cat meows\n"; }
+};
+
+class BabyDog : public Dog {
+
+    public:
+        // void speak() override { cout << "Idk";}           cannot override final since speak in Dog is final
+};
+
